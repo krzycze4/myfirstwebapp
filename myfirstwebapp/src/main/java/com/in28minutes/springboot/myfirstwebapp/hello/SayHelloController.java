c7f55214ba94d4ebec8c5c7d131ce01b55ba65ff
krzycze4 <108796169+krzycze4@users.noreply.github.com>
2023-05-18 11:56:45 +0200

    Step 11 Simple Logging Form

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -1,18 +1,36 @@
 package com.in28minutes.springboot.myfirstwebapp.hello;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class SayHelloController {
 	
-	@RequestMapping("/say-hello")
+	@RequestMapping("say-hello")
+	@ResponseBody
 	public String sayHello() {
 		return "Hello! What are you learning today?";
 	}
 
-	@RequestMapping("/say-hello-html")
+	@RequestMapping("say-hello-html")
+	@ResponseBody
 	public String sayHelloHtml() {
-		return "Hello! What are you learning today?";
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title> My First HTML Page - Changed</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("My first html page with body - Changed");
+		sb.append("</body>");
+		sb.append("</html>");
+		
+		return sb.toString();
+	}
+
+	@RequestMapping("say-hello-jsp")
+	public String sayHelloJsp() {
+		return "sayHello";
 	}
 }  
